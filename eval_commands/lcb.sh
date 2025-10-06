03ff0dbc292155834154ac121da260d89bedd2a8
Neelectric <Neel.R@web.de>
2025-10-06 16:34:23 +0100

    fix lcb again

--- eval_commands/lcb.sh
+++ eval_commands/lcb.sh
@@ -3,10 +3,10 @@ NUM_GPUS=1
 MODEL=meta-llama/Llama-3.1-8B-Instruct
 
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.85,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-TASK=math_500
+TASK=lcb:codegeneration
 OUTPUT_DIR=data/evals/$REVISION
 
-VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
+VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "extended|$TASK|0|0" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR 
 
