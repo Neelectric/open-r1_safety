69483becdfced007c71911017265864d10920a38
Neelectric <Neel.R@web.de>
2025-10-07 12:03:31 +0100

    let's re eval llama base on lcb

--- eval_commands/lcb.sh
+++ eval_commands/lcb.sh
@@ -1,14 +1,15 @@
 # LiveCodeBench
 NUM_GPUS=1
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_MoT_codev00.03
+# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_MoT_codev00.03
+MODEL=meta-llama/Llama-3.1-8B-Instruct
 
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.85,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 TASK=lcb:codegeneration
 OUTPUT_DIR=data/evals/
 
-# VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "extended|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR 
+VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "extended|$TASK|0|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR 
 
 
 # #2k
@@ -54,12 +55,12 @@ OUTPUT_DIR=data/evals/
 #     --use-chat-template \
 #     --output-dir $OUTPUT_DIR 
 
-#10k
-REVISION=v00.03-step-000010384
-MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.85,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-TASK=lcb:codegeneration
-OUTPUT_DIR=data/evals/$REVISION
+# #10k
+# REVISION=v00.03-step-000010384
+# MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.85,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+# TASK=lcb:codegeneration
+# OUTPUT_DIR=data/evals/$REVISION
 
-VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "extended|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR 
+# VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "extended|$TASK|0|0" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR 
