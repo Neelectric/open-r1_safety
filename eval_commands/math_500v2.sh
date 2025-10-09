f6cafd51aaaa1f9fddd33f3f7c11084650298316
Neelectric <Neel.R@web.de>
2025-10-09 20:41:25 +0100

    update with dir

--- eval_commands/math_500v2.sh
+++ eval_commands/math_500v2.sh
@@ -3,7 +3,7 @@ TASK=math_500
 OUTPUT_DIR=data/evals/
 
 VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm \
-    vllm_config.yaml \
+    eval_commands/vllm_config.yaml \
     "lighteval|$TASK|0|0" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
