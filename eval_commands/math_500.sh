c7d9dfaa0368c2c0456e621bead3d8402f489278
Neelectric <Neel.R@web.de>
2025-10-17 16:57:58 +0100

    reverting back to old flash attn install...

--- eval_commands/math_500.sh
+++ eval_commands/math_500.sh
@@ -1,12 +1,13 @@
-NUM_GPUS=1
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_MoT_mathv00.04
+NUM_GPUS=4
+# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_MoT_mathv00.04
+MODEL=/gpfs/projects/ehpc283/repos/open-r1/data/Llama-3.1-8B-Instruct_MoT_SFT
 
 # base
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 TASK=math_500
 OUTPUT_DIR=data/evals/
 
-VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+HF_HUB_OFFLINE=1 VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
 
