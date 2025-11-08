1c4a049d3e82e893ec583f57c589515c4612c993
Neelectric <Neel.R@web.de>
2025-11-08 20:41:32 +0000

    update

--- eval_commands/math_500v2.sh
+++ eval_commands/math_500v2.sh
@@ -5,9 +5,12 @@ uv pip install more_itertools
 export PYTHONPATH="${PYTHONPATH}:$(dirname "$0")"
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 NUM_GPUS=1
+MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.07
+$REVISION=Main
+MAX_TOKS=4096
 
 lighteval vllm \
-  "model_name=Neelectric/Llama-3.1-8B-Instruct_GRPO_Math-220k_v00.11,revision=checkpoint-207,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.92,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}" \
+  "model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$MAX_TOKS,gpu_memory_utilization=0.92,generation_parameters={max_new_tokens:$MAX_TOKS,temperature:0.6,top_p:0.95}" \
   "custom|math_500_n1|0|0" \
   --custom-tasks math_500_single \
   --use-chat-template \
