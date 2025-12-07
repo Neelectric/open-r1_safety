eb6b4e159d57f970a03cf2e24333ece5a3770895
Neelectric <Neel.R@web.de>
2025-12-07 00:05:04 +0000

    update math_500 to eval v00.14

--- eval_commands/math_500.sh
+++ eval_commands/math_500.sh
@@ -1,11 +1,15 @@
 uv pip install vllm==0.8.5
 uv pip install more_itertools
-NUM_GPUS=1
-# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_MoT_mathv00.04
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.12
-#REVISION=v00.10-step-000016155
-REVISION=main
+
+NUM_GPUS=4
 NUM_TOKS=4096
+
+# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_MoT_mathv00.04
+#REVISION=v00.10-step-000016155
+
+MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.14
+REVISION=main
+
 
 # base
 MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
@@ -13,13 +17,13 @@ TASK=math_500
 OUTPUT_DIR=data/evals/
 
 VLLM_WORKER_MULTIPROC_METHOD="spawn" \
-CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+CUDA_VISIBLE_DEVICES="0,1,2,3" \
 TORCHINDUCTOR_CACHE_DIR=./.cache/${CUDA_VISIBLE_DEVICES}/ \
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
 
-uv pip install vllm==0.10.2
+uv pip install vllm==0.11.0
 
 # #1k
 # REVISION=v00.01-step-000001172
