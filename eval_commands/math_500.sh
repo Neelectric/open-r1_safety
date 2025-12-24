cd4fa392b6fe4671a573047e336b6e6fec2899b8
Neelectric <Neel.R@web.de>
2025-12-24 10:36:12 +0000

    eval

--- eval_commands/math_500.sh
+++ eval_commands/math_500.sh
@@ -3,73 +3,25 @@ uv pip install vllm==0.8.5
 uv pip install more_itertools
 uv pip install datasets==2.21.0
 
-NUM_GPUS=4
+NUM_GPUS=2
 NUM_TOKS=4096
 
-# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_MoT_mathv00.04
-#REVISION=v00.10-step-000016155
-
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.16
-REVISION=main
+MODEL=/home/user/.cache/huggingface/hub/models--Neelectric--Llama-3.1-8B-Instruct_GRPO_Math-220kv00.05/snapshots/b518a2683b6efc37ee136630c6b3d81c832a277a
+REVISION=v00.05-step-000000486
 
 
-# base
-MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
+# 324
+MODEL_ARGS="model_name=$MODEL,revision=\"$REVISION\",dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 TASK=math_500
 OUTPUT_DIR=data/evals/
 
 VLLM_WORKER_MULTIPROC_METHOD="spawn" \
-CUDA_VISIBLE_DEVICES="0,1,2,3" \
+CUDA_VISIBLE_DEVICES="0,1" \
 TORCHINDUCTOR_CACHE_DIR=./.cache/${CUDA_VISIBLE_DEVICES}/ \
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
 
+
+
 uv pip install vllm==0.11.0
-
-# #1k
-# REVISION=v00.01-step-000001172
-# MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-# OUTPUT_DIR=data/evals/$REVISION
-
-# VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
-
-# #2k
-# REVISION=v00.01-step-000002344
-# MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-# OUTPUT_DIR=data/evals/$REVISION
-
-# VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
-
-# #3k
-# REVISION=v00.01-step-000003516
-# MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-# OUTPUT_DIR=data/evals/$REVISION
-
-# VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
-
-
-# #4k
-# REVISION=v00.01-step-000004688
-# MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-# OUTPUT_DIR=data/evals/$REVISION
-
-# VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
-
-
-# #5859k
-# REVISION=v00.01-step-000005859
-# MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-# OUTPUT_DIR=data/evals/$REVISION
-
-# VLLM_WORKER_MULTIPROC_METHOD=spawn lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
