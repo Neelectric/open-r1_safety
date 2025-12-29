39d2d6c26f34c9daeada8c18224c5433030de3c3
Neelectric <Neel.R@web.de>
2025-12-29 22:54:32 +0000

    making progress, will have to bump lighteval to access ifbench

--- eval_commands/ifeval_ifbench.sh
+++ eval_commands/ifeval_ifbench.sh
@@ -8,12 +8,13 @@ NUM_TOKS=4096
 
 # MODEL=/root/.cache/huggingface/hub/models--Neelectric--Llama-3.1-8B-Instruct_GRPO_Math-220kv00.10/snapshots/afbdbaa07751effee4a6f40b2d5b77ea1a876435
 # REVISION=main-step-000000275
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.10
+# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.10
+MODEL=meta-llama/Llama-3.1-8B-Instruct
 REVISION=main
 
 
 # ifbench_test
-MODEL_ARGS="model_name=$MODEL,revision=\"$REVISION\",dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
+MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=data/evals/
 
 
