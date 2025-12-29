be01caeaf8c0824e275e165f96a44a369215ba61
Neelectric <Neel.R@web.de>
2025-12-29 16:54:18 +0000

    IFBench is a good option here

--- eval_commands/ifeval_ifbench.sh
+++ eval_commands/ifeval_ifbench.sh
@@ -6,22 +6,40 @@ uv pip install datasets==2.21.0
 NUM_GPUS=1
 NUM_TOKS=4096
 
-MODEL=/root/.cache/huggingface/hub/models--Neelectric--Llama-3.1-8B-Instruct_GRPO_Math-220kv00.10/snapshots/afbdbaa07751effee4a6f40b2d5b77ea1a876435
-REVISION=main-step-000000275
+# MODEL=/root/.cache/huggingface/hub/models--Neelectric--Llama-3.1-8B-Instruct_GRPO_Math-220kv00.10/snapshots/afbdbaa07751effee4a6f40b2d5b77ea1a876435
+# REVISION=main-step-000000275
+MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.10
+REVISION=main
 
 
-# 324
+# ifbench_test
 MODEL_ARGS="model_name=$MODEL,revision=\"$REVISION\",dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
-# TASK=math_500
-TASK=aime24
 OUTPUT_DIR=data/evals/
 
+
+
+
+TASK=ifbench_test
 VLLM_WORKER_MULTIPROC_METHOD="spawn" \
 CUDA_VISIBLE_DEVICES="0" \
 TORCHINDUCTOR_CACHE_DIR=./.cache/${CUDA_VISIBLE_DEVICES}/ \
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
+
+TASK=ifeval
+VLLM_WORKER_MULTIPROC_METHOD="spawn" \
+CUDA_VISIBLE_DEVICES="0" \
+TORCHINDUCTOR_CACHE_DIR=./.cache/${CUDA_VISIBLE_DEVICES}/ \
+lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
+
+
+
+
+
+
 
 
 
