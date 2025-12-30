f94f735bdd856c572e65690788600f62effbc0a1
Neelectric <Neel.R@web.de>
2025-12-30 14:20:08 +0000

    h200 commit

--- eval_commands/ifeval_ifbench.sh
+++ eval_commands/ifeval_ifbench.sh
@@ -1,8 +1,8 @@
-uv pip install lighteval==0.13.0
-uv pip install vllm==0.10.1
+# uv pip install lighteval==0.13.0
+# uv pip install vllm==0.10.1
 uv pip install more_itertools
 uv pip install syllapy
-# uv pip install spacy[ja,ko,th]>=3.8.0
+uv pip install spacy[ja,ko,th]>=3.8.0
 uv pip install emoji
 
 NUM_GPUS=4
