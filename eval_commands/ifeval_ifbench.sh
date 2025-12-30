41e565ca1d92d636919a9871134824bba4627ffd
Neelectric <Neel.R@web.de>
2025-12-30 00:05:26 +0000

    let's try v11 with z ds-2

--- eval_commands/ifeval_ifbench.sh
+++ eval_commands/ifeval_ifbench.sh
@@ -1,9 +1,11 @@
-uv pip install lighteval==0.9.2
-uv pip install vllm==0.8.5
+uv pip install lighteval==0.13.0
+uv pip install vllm==0.10.0
 uv pip install more_itertools
-uv pip install datasets==2.21.0
+uv pip install syllapy
+uv pip install spacy[ja,ko,th]>=3.8.0
+uv pip install emoji
 
-NUM_GPUS=1
+NUM_GPUS=4
 NUM_TOKS=4096
 
 # MODEL=/root/.cache/huggingface/hub/models--Neelectric--Llama-3.1-8B-Instruct_GRPO_Math-220kv00.10/snapshots/afbdbaa07751effee4a6f40b2d5b77ea1a876435
@@ -17,32 +19,9 @@ REVISION=main
 MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=data/evals/
 
-
-
-
-TASK=ifbench_test
 VLLM_WORKER_MULTIPROC_METHOD="spawn" \
-CUDA_VISIBLE_DEVICES="0" \
-TORCHINDUCTOR_CACHE_DIR=./.cache/${CUDA_VISIBLE_DEVICES}/ \
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
+lighteval vllm $MODEL_ARGS ifbench_test,ifeval \
     --output-dir $OUTPUT_DIR
-
-TASK=ifeval
-VLLM_WORKER_MULTIPROC_METHOD="spawn" \
-CUDA_VISIBLE_DEVICES="0" \
-TORCHINDUCTOR_CACHE_DIR=./.cache/${CUDA_VISIBLE_DEVICES}/ \
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
-
-
-
-
-
-
-
 
 
 uv pip install vllm==0.11.2
-uv pip install datasets==4.4.2
