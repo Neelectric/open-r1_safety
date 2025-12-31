b5de4d72ee03c1d580bdb0477ea5e30686de687c
Neelectric <Neel.R@web.de>
2025-12-31 15:21:48 +0000

    fix

--- eval_commands/math_500.sh
+++ eval_commands/math_500.sh
@@ -17,7 +17,7 @@ TASK=math_500
 OUTPUT_DIR=data/evals/
 
 VLLM_WORKER_MULTIPROC_METHOD="spawn" \
-lighteval vllm $MODEL_ARGS "lighteval|$TASK" \
+lighteval vllm $MODEL_ARGS $TASK \
     --output-dir $OUTPUT_DIR
 
 
