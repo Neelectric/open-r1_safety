d8fa2ff52ea4a026ef96e0250a4ce59c914e9d3b
Neelectric <Neel.R@web.de>
2026-01-02 15:22:18 +0000

    making progress

--- eval_commands/math_500.sh
+++ eval_commands/math_500.sh
@@ -1,17 +1,19 @@
 # uv pip install lighteval==0.13.0
-uv pip install vllm==0.10.1
+# uv pip install vllm==0.10.1
 # uv pip install -U datasets
-# uv pip install more_itertools
+uv pip install more_itertools
 
-NUM_GPUS=5
+NUM_GPUS=1
 NUM_TOKS=4096
 
-MODEL=/root/.cache/huggingface/hub/models--Neelectric--Llama-3.1-8B-Instruct_GRPO_Math-220kv00.10/snapshots/e2583dfaba3d8acc831710720ff608b0fac3c4a9
-REVISION=main-step-000000025
+# MODEL=/root/.cache/huggingface/hub/models--Neelectric--Llama-3.1-8B-Instruct_GRPO_Math-220kv00.10/snapshots/e2583dfaba3d8acc831710720ff608b0fac3c4a9
+# REVISION=main-step-000000025
+MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Math-220kv00.10
+REVISION=main
 
 
 # 324
-MODEL_ARGS="model_name=$MODEL,revision=\"$REVISION\",dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
+MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 TASK=math_500
 # TASK=aime24
 OUTPUT_DIR=data/evals/
