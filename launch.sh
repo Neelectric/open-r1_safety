2051da8ba75fc9bbd252a26fc8eb3bac00bb7208
Neelectric <Neel.R@web.de>
2026-01-26 22:06:43 +0000

    v5 distill

--- launch.sh
+++ launch.sh
@@ -34,16 +34,21 @@ while true; do
 done
 
 # Launch fisher
+uv pip install vllm==0.11.2
 # VERSION=fisher_v00.03 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft/config_distill_fisher_v00.03.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
 
 # launch chat sft
 VERSION=v00.04 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_chat/config_distill_v00.04.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
 
+VERSION=v00.05 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_chat/config_distill_v00.05.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
+
+
+
 # bash eval_commands/math_500.sh Neelectric/Llama-3.1-8B-Instruct_SFT_Chat-220kv00.01 main
 
-# uv pip install vllm==0.10.1
-# uv pip uninstall flashinfer-python
-# uv pip install more_itertools syllapy "spacy[ja,ko,th]>=3.8.0" emoji "numpy==2.2"
+uv pip install vllm==0.10.1
+uv pip uninstall flashinfer-python
+uv pip install more_itertools syllapy "spacy[ja,ko,th]>=3.8.0" emoji "numpy==2.2"
 
 # MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Chat-220kv00.03
 # VERSION=v00.03
@@ -60,4 +65,15 @@ VERSION=v00.04 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_chat/conf
 # bash eval_commands/ifeval_ifbench.sh $MODEL $VERSION-step-000039339
 # bash eval_commands/ifeval_ifbench.sh $MODEL main
 
-# uv pip install vllm==0.11.2
+# 4
+MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Chat-220kv00.04
+VERSION=main
+bash eval_commands/ifeval_ifbench.sh $MODEL $VERSION
+
+# 5
+MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_Chat-220kv00.05
+VERSION=main
+bash eval_commands/ifeval_ifbench.sh $MODEL $VERSION
+
+
+uv pip install vllm==0.11.2
