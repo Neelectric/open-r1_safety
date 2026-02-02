3503f2fcec66d0d07cf7951b9ad771aa0fe38272
Neelectric <Neel.R@web.de>
2026-02-02 17:22:50 +0000

    Starting 8k MoT run, 269,513 samples (2 epochs, ga=4, bsz=6, total_bsz=96, total optim steps=5,616)

--- launch.sh
+++ launch.sh
@@ -38,17 +38,7 @@ done
 # uv pip install vllm==0.11.2
 
 # launch code sft
-# VERSION=v00.04 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.04.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
-
-# VERSION=v00.05 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.05.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
-
-# VERSION=v00.06 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.06.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
-
-# VERSION=v00.07 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.07.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
-
-# VERSION=v00.08 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.08.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
-
-# VERSION=v00.09 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.09.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
+# VERSION=v00.02 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_mot/config_distill_v00.02.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
 
 
 uv pip install vllm==0.10.1
@@ -73,17 +63,17 @@ MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.06
 VERSION=main
 bash eval_commands/gpqa_diamond.sh $MODEL $VERSION
 
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.07
-VERSION=main
-bash eval_commands/gpqa_diamond.sh $MODEL $VERSION
+# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.07
+# VERSION=main
+# bash eval_commands/gpqa_diamond.sh $MODEL $VERSION
 
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.08
-VERSION=main
-bash eval_commands/gpqa_diamond.sh $MODEL $VERSION
+# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.08
+# VERSION=main
+# bash eval_commands/gpqa_diamond.sh $MODEL $VERSION
 
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.09
-VERSION=main
-bash eval_commands/gpqa_diamond.sh $MODEL $VERSION
+# MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.09
+# VERSION=main
+# bash eval_commands/gpqa_diamond.sh $MODEL $VERSION
 
 
 
