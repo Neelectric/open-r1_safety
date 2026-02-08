d7ea37a9ba417ed33061bf0528d3952cb25bbcec
Neelectric <Neel.R@web.de>
2026-02-08 22:54:46 +0000

    new replay configs and viz

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 source /root/openr1_v2/bin/activate
 
 # Configuration
-CHECK_INTERVAL=3000  # seconds between checks
+CHECK_INTERVAL=300  # seconds between checks
 MEMORY_THRESHOLD=500  # MB - GPUs with less memory used are considered free
 UTIL_THRESHOLD=5  # % - GPUs with less utilization are considered free
 
@@ -39,9 +39,8 @@ uv pip install vllm==0.11.2
 # VERSION=fisher_v00.03 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_fisher_v00.03.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
 
 # Launch vanilla SFT
-# VERSION=v00.10 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.10.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
+# VERSION=v00.15 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.15.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
 
-# VERSION=v00.14 envsubst < recipes/meta-llama/Llama-3.1-8B-Instruct/sft_science/config_distill_v00.14.yaml > temp_config.yaml && accelerate launch --config_file recipes/accelerate_configs/zero1_claude.yaml --num_processes=4 src/open_r1/sft.py --config temp_config.yaml
 
 # launch evals
 uv pip install vllm==0.10.1
@@ -50,17 +49,9 @@ uv pip install more_itertools syllapy "spacy[ja,ko,th]>=3.8.0" emoji "numpy==2.2
 
 VERSION=main
 
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.11
-bash eval_commands/gpqa.sh $MODEL $VERSIONj
-
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.12
+MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.15
 bash eval_commands/gpqa.sh $MODEL $VERSION
 
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.13
-bash eval_commands/gpqa.sh $MODEL $VERSION
-
-MODEL=Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.14
-bash eval_commands/gpqa.sh $MODEL $VERSION
 
 # bash eval_commands/math_500.sh $MODEL $VERSION
 
@@ -69,4 +60,4 @@ bash eval_commands/gpqa.sh $MODEL $VERSION
 
 
 uv pip install vllm==0.11.2
-# uv run fisher_testbed/vllm_inference.py -m Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.01 -mm 4096
+# uv run fisher_testbed/vllm_inference.py -m Neelectric/Llama-3.1-8B-Instruct_SFT_sciencev00.15 -mm 4096
