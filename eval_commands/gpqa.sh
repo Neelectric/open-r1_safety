e31051466bd135e548b4989ea0f93b5c7439b51a
s2011847-eidf107 <Neel.Rajani@ed.ac.uk>
2026-02-16 09:56:23 +0000

    More evals and viz

--- eval_commands/gpqa.sh
+++ eval_commands/gpqa.sh
@@ -2,6 +2,8 @@ MODEL_ID="${1:-Neelectric/Llama-3.2-1B-Instruct_SFT_Math-220kvdebug_00.04}"
 REVISION="${2:-main}"
 NUM_GPUS="${3:-4}"
 
+echo "Model: $MODEL_ID | Revision: $REVISION | GPUs: $NUM_GPUS"
+
 uv pip install vllm==0.10.1
 uv pip install flashinfer-python
 uv pip install more_itertools
